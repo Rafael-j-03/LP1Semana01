cd47c2e5abed6e1dd31a0dd7d344b9feecbbd42e
Rafael-j-03 <ra.david@hotmail.com>
2023-03-02 16:54:35 +0000

    Exercise 3
    Add project 'TiposVarios'
    
    Add integrals variables and print them

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Integral variables
+            byte number1 = 1;
+            sbyte number2 = -1;
+            int number3 = 65536;
+
+            // Print the integral variables
+            Console.WriteLine(number1);
+            Console.WriteLine(number2);
+            Console.WriteLine(number3);
+        }
+    }
+}
