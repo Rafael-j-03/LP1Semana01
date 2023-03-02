a7c18ef0d2b50db35b8533719f5ccd7cdfa7a4ae
Rafael-j-03 <ra.david@hotmail.com>
2023-03-02 21:19:40 +0000

    Exercise 4
    Add project 'VariasStrings'
    
    Add multiple string variables, normal and @(verbatim), and print them

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // String variables
+            string s1 = "Hello World \u2605";
+            string s2 = @"Hello World \u2605";
+            string s3 = "This\t is\t a\t test\t \u00A9";
+            string s4 = @"This\t is\t a\t test\t \u00A9";
+            string s5 = "C:\\Users\\Rafael\\Downloads";
+            string s6 = @"C:\Users\Rafael\Downloads";
+
+            // Print the string variables
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
+            Console.WriteLine(s4);
+            Console.WriteLine(s5);
+            Console.WriteLine(s6);
+        }
+    }
+}
