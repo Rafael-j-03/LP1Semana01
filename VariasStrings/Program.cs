09075606ea48e13fdcbc3f59167606513a51c421
Rafael-j-03 <ra.david@hotmail.com>
2023-03-02 21:41:51 +0000

    Exercise 5
    Add concatenation of strings
    
    Add interpolation of strings
    
    Add string formats
    
    Add string formats in console writing

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -21,6 +21,33 @@ namespace VariasStrings
             Console.WriteLine(s4);
             Console.WriteLine(s5);
             Console.WriteLine(s6);
+
+
+            int x = 5;
+            int y = 3;
+            // Concatenation of strings
+            string s7 = "a" + "b" + "c" + "=" + "abc";
+            string s8 = "a" + x;
+            
+            // Interpolation of strings
+            string s9 = $"The x value is: {x}";
+            string s10 = $"x + y is equal to: {x + y}";
+
+            // String formats
+            string s11 = String.Format("x is {0} and y {1}", x, y);
+            string s12 = String.Format("x is {1} and y {0}", 1, -2);
+
+            // Print all of the new strings
+            Console.WriteLine(s7);
+            Console.WriteLine(s8);
+            Console.WriteLine(s9);
+            Console.WriteLine(s10);
+            Console.WriteLine(s11);
+            Console.WriteLine(s12);
+
+            // String formats in console writing
+            Console.WriteLine("z value is {0}", 3);
+            Console.WriteLine(@"Verbatim x value is {0}", x);
         }
     }
 }
