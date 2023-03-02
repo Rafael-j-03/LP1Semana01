1de65622339bb6c2a6f7d95ec4d7861870f75cd8
Rafael-j-03 <ra.david@hotmail.com>
2023-03-02 16:56:49 +0000

    Exercise 3
    Add char variables and print them

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -15,6 +15,16 @@ namespace TiposVarios
             Console.WriteLine(number1);
             Console.WriteLine(number2);
             Console.WriteLine(number3);
+
+            // Char variables
+            char star = '\u272D';
+            char lucky7 = '\u277C';
+            char laoEllipsis = '\u0EAF';
+
+            // Print the char variables
+            Console.WriteLine(star);
+            Console.WriteLine(lucky7);
+            Console.WriteLine(laoEllipsis);
         }
     }
 }
