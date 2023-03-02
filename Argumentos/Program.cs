ad0a86b87ae6d43accec9dba854d6438902c3e75
Rafael-j-03 <ra.david@hotmail.com>
2023-03-02 16:44:11 +0000

    Exercise 1
    Add 'Bye World'
    
    Add loop for args

--- Argumentos/Program.cs
+++ Argumentos/Program.cs
@@ -6,7 +6,13 @@ namespace Argumentos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Bye World");
+            
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine(args[i]);
+            }
         }
     }
 }
